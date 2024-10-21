06b534ed89984c4daa556354be9df5bf48f55673
malak hmandi <malakhmandi02@gmail.com>
2024-10-21 20:08:47 +0200

    microservices

---
+++ src/main/java/com/malak/users/service/UserService.java
@@ -0,0 +1,20 @@
+package com.malak.users.service;
+
+import java.util.List;
+
+import com.malak.users.entities.Role;
+import com.malak.users.entities.User;
+
+public interface UserService {
+	User saveUser(User user);
+
+	User findUserByUsername(String username);
+
+	Role addRole(Role role);
+
+	User addRoleToUser(String username, String rolename);
+
+	List<User> findAllUsers();
+
+}
+
